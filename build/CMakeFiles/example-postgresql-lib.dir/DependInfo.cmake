75c5a57193a8cbe859b3d93a11d5bff18cd7a096
admin <admin@Alushta.myguest.virtualbox.org>
2023-10-21 12:09:20 +0300

    first commit

---
+++ build/CMakeFiles/example-postgresql-lib.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/admin/VSCode/oatpp/example-postgresql/src/ErrorHandler.cpp" "CMakeFiles/example-postgresql-lib.dir/src/ErrorHandler.cpp.o" "gcc" "CMakeFiles/example-postgresql-lib.dir/src/ErrorHandler.cpp.o.d"
+  "/home/admin/VSCode/oatpp/example-postgresql/src/service/UserService.cpp" "CMakeFiles/example-postgresql-lib.dir/src/service/UserService.cpp.o" "gcc" "CMakeFiles/example-postgresql-lib.dir/src/service/UserService.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
